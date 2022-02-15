e533301aec45069e14250e6d0ebc04cebfc30fa2
Ryan Binns <r@rbi.nz>
2022-02-16 00:29:47 +0700

    refactor for unit testing

--- src/routes/converter.controller.js
+++ src/routes/converter.controller.js
@@ -1,3 +1,5 @@
+import { convertCurrencies } from "./converter.service";
+
 const exchangeRates = {
   USD: 1,
   EUR: 0.87815,
@@ -11,25 +13,12 @@ const exchangeRates = {
   COP: 3203.18,
 };
 
-// todo: move to service file
-export const convertCurrency = (value, sourceRate, targetRate) =>
-  (value * 100000) / ((sourceRate * 100000) / (targetRate * 100000)) / 100000;
-
-export const round = (value) => Math.round(value * 100) / 100;
-
-export const getRate = currency => exchangeRates[currency.toUpperCase()];
 
 export const currencyConverter = (req, res) => {
   const { params, body } = req;
   const currency = params.currency.toUpperCase();
   const sources = Object.entries(body);
-  const result = sources.reduce((acc, [sourceCurrency, value]) => 
-      acc + convertCurrency(
-      value,
-      getRate(sourceCurrency),
-      getRate(currency)
-    ), 0
-  );
-  const roundedResult = Math.round(result * 100) / 100
-  res.json({ [currency]: roundedResult });
+  // todo: sanitise inputs
+  const result = convertCurrencies(sources, currency)
+  res.json({ [currency]: result });
 };
--- src/routes/converter.service.js
+++ src/routes/converter.service.js
@@ -11,10 +11,19 @@ const exchangeRates = {
   COP: 3203.18,
 };
 
-// todo: move to service file
-export const calculateExchangeRates = (value, sourceRate, targetRate) =>
+export const convertCurrency = (value, sourceRate, targetRate) =>
   (value * 100000) / ((sourceRate * 100000) / (targetRate * 100000)) / 100000;
 
 export const round = (value) => Math.round(value * 100) / 100;
 
-export const convertCurrency = () => {};
+export const getRate = (currency) => exchangeRates[currency.toUpperCase()];
+
+export const convertCurrencies = (sourceData, targetCurrency) => {
+  const result = sourceData.reduce((acc, [sourceCurrency, value]) => {
+    return (
+      acc +
+      convertCurrency(value, getRate(sourceCurrency), getRate(targetCurrency))
+    );
+  }, 0);
+  return round(result);
+}
